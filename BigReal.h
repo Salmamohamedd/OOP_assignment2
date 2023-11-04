02112251308ea8637e4deba81cf4d60f4ba107df
Salmamohamedd <125445879+Salmamohamedd@users.noreply.github.com>
2023-11-04 10:33:53 +0200

    Create BigReal.h

---
+++ BigReal.h
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef BIGREAL_BIGREAL_H
+#define BIGREAL_BIGREAL_H
+
+class BigReal{
+private:
+    char sign = '+';
+    string integer, fraction;
+    //bool ValidReal;
+public:
+//    void setValid(string);
+//    bool getValid;
+    bool isValidReal(string);
+    BigReal(string);
+    BigReal operator+ (BigReal& anotherReal );
+    BigReal operator- (BigReal& anotherReal);
+    bool operator< (BigReal anotherReal);
+    bool operator> (BigReal anotherReal);
+    bool operator== (BigReal anotherReal);
+    friend ostream& operator << (ostream& out, BigReal num);
+
+    void print();
+};
+
+#endif //BIGREAL_BIGREAL_H
