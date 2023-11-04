1b2ec1f6c5eb5493e4e7e21a7cf0e3b758665d99
Salmamohamedd <125445879+Salmamohamedd@users.noreply.github.com>
2023-11-04 12:52:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,18 +6,25 @@ using namespace std;
 
 
 int main() {
-    BigReal r1("+333.9999");
-    BigReal r2("-.777");
-    BigReal r3("33.38.8");
+    BigReal r1("-333.9999");
+    BigReal r2("1233.777");
+    BigReal r3("3338.8");
     BigReal r4("0.");
 
 
-    r1.print();
-    r2.print();
-    r3.print();
-    r4.print();
+//    r1.print();
+//    r2.print();
+//    r3.print();
+//    r4.print();
 
-    r1.isValidReal("+333.9999") ? cout << "True" : cout << "false";
+  //  r1.isValidReal("+333.9999") ? cout << "True" : cout << "false";
+
+    if(r1 < r2){
+        cout <<"\n";
+    }
+    if(r2 < r3){
+        cout << "\n";
+    }
 
     return 0;
 }
