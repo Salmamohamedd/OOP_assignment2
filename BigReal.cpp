f638f214765ad3df1a6b6645489c1ead88bf9b46
Salmamohamedd <125445879+Salmamohamedd@users.noreply.github.com>
2023-11-04 10:33:02 +0200

    Update BigReal.cpp

--- BigReal.cpp
+++ BigReal.cpp
@@ -1,49 +1,71 @@
 #include <bits/stdc++.h>
+#include "BigReal.h"
 #include <regex>
+
+
 using namespace std;
 
-class BigReal{
-private:
-    char sign = '+';
-    string integer, fraction;
-public:
-    BigReal(string);
-    BigReal operator+ (BigReal&);
-    void print();
-};
-
+//maybe create a default constructor, making value = 0.0?
 BigReal::BigReal(string real) {
-    if (regex_match(real, regex("[+-]?[0-9]+?.?[0-9]+?"))) {
+    if (isValidReal(real)) {
         if (real[0] == '-') {
             sign = '-';
-            real[0] = '0';
+            real.erase(0, 1);
         } else if (real[0] == '+') {
-            real[0] = '0';
+            sign = '+';
+            real.erase(0, 1);
         }
+        if (real.find('.') != -1) {
+            integer = real.substr(0, real.find("."));
+            fraction = real.substr(integer.size() + 1, real.find('\0') - integer.size() - 1);
+        } else {
+            integer = real.substr(0, real.find('\0'));
+            fraction = '0';
+        }
+    } else {
+        integer = '0';
+        fraction = '0';
     }
 
-    if (integer.empty())
-        integer = '0';
-    if (fraction.empty())
-        fraction = '0';
 }
 
+
+bool BigReal::isValidReal(string real) {
+    if (regex_match(real, regex("[-+]?\\d*.?\\d+"))) {
+      //  cout << real << " is a Valid BigReal number!\n";
+        return true;
+    } else {
+        //cout << real << " is an InValid BigReal number!\n";
+        return false;
+    }
+}
 
 void BigReal::print() {
     cout << sign << integer << "." << fraction << "\n";
 }
 
-int main() {
-    BigReal r1("+333.9999");
-    BigReal r2("-.777");
-    BigReal r3("33.38.8");
-    BigReal r4("0.9999x");
+BigReal BigReal::operator+(BigReal &anotherReal) {
+    string result="";
+    int common,temp, carry=0;
+    if (fraction.size()> anotherReal.fraction.size()){
+        result=fraction.substr((anotherReal.fraction.size())+1, fraction.size()-anotherReal.fraction.size());
+        common=anotherReal.fraction.size();
+    }else if(fraction.size()<anotherReal.fraction.size()){
+        result=anotherReal.fraction.substr((fraction.size())+1, anotherReal.fraction.size()-fraction.size());
+        common= fraction.size();
+    }else{
+        common=fraction.size();
+    }
+    for (int i = common-1; i >=0 ; --i) {
+        temp = carry + fraction[i] + anotherReal.fraction[i] - '0';
+        if (temp > '9'){
+            carry=1;
+            temp -= 10;
+        }else {
+            carry =0;
+        }
+        //result=temp +result;
+    }
 
-
-    r1.print();
-    r2.print();
-    r3.print();
-    r4.print();
-
-    return 0;
+    //fraction + anotherReal.fraction;
 }
