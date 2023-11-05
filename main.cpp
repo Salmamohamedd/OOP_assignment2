53bc17bec5aaebe057adff9c8792e56a7dd5ba33
YasmineS21 <112911384+YasmineS21@users.noreply.github.com>
2023-11-05 12:11:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,29 +2,47 @@
 #include "BigReal.h"
 
 #include <regex>
+
 using namespace std;
 
 
 int main() {
-    BigReal r1("-333.9999");
-    BigReal r2("1233.777");
-    BigReal r3("3338.8");
-    BigReal r4("0.");
+    BigReal r1("-56.8");
+    BigReal r2("-123.55");
+    BigReal r3("56.8");
+    BigReal r4("123.55");
+
+    cout << r1 + r1 << '\n'; //-113.6 (CORRECT WITH WRONG EXIT CODE) (-ve + -ve ) same number
+    cout << r1 + r2 << '\n'; //-180.35 (CORRECT WITH WRONG EXIT CODE) (-ve + -ve) diff nums
+    cout << r1 + r3; //0 (NO OUTPUT, WRONG EXIT CODE) (-ve + +ve) same number ////////
+    cout << "\n";
+    cout << r1 + r4; //66.75 (WORKING) (-ve + +ve) diff nums
+    cout << "\n";
+    cout << r2 + r1;// -180.35 (CORRECT WITH WRONG EXIT CODE)
+    cout << "\n";
+    cout << r2 + r2;//-247.1 (CORRECT WITH WRONG EXIT CODE)
+    cout << '\n';
+    cout << r2 + r3 << '\n';// -66.75 (WORKING)
+    cout << r2 + r4; // 0 (NO OUTPUT WRONG EXIT CODE)
+    cout << "\n";
+    cout << r3 + r1; // 0 (NO OUTPUT WITH WRONG EXIT CODE) //////////
+    cout << "\n";
+    cout << r3 + r2; // -66.75 (WORKING)
+    cout << "\n";
+    cout << r3 + r3; // 113.6 (CORRECT WITH WRONG EXIT CODE)
+    cout << "\n";
+    cout << r3 + r4; // 180.35 (CORRECT WITH WRONG EXIT CODE)
+    cout << "\n";
+    cout << r4 + r1; // 66.75 (WORKING)
+    cout << "\n";
+    cout << r4 + r2; // 0 (NO OUTPUT, WRONG EXIT CODE)
+    cout << "\n";
+    cout << r4 + r3; // 180.35 (CORRECT WITH WRONG EXIT CODE)
+    cout << "\n";
+    cout << r4 + r4; //247.1 (CORRECT WITH WRONG EXIT CODE)
 
 
-//    r1.print();
-//    r2.print();
-//    r3.print();
-//    r4.print();
 
-  //  r1.isValidReal("+333.9999") ? cout << "True" : cout << "false";
-
-    if(r1 < r2){
-        cout <<"\n";
-    }
-    if(r2 < r3){
-        cout << "\n";
-    }
 
     return 0;
 }
